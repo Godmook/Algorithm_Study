ca952b5436a245693b3028162af823ee8b47a4c9
Godmook <cmoh4135@naver.com>
2022-08-01 05:44:52 +0900

    Modify

---
+++ Algorithm_Study/src/Baekjoon/P_10814.java
@@ -0,0 +1,24 @@
+package Baekjoon;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+
+public class P_10814 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int a=Integer.parseInt(br.readLine());
+        String [][]array=new String[a][2];
+        for(int i=0;i<a;i++){
+            array[i]=br.readLine().split(" ");
+        }
+
+        Arrays.sort(array, Comparator.comparingInt(o -> Integer.parseInt(o[0])));
+        for(int i=0;i<a;i++){
+            bw.write(array[i][0]+" "+array[i][1]+"\n");
+        }
+        bw.flush();
+    }
+}
