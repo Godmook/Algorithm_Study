e07f8820b1bd9aac2acde0b4b6f37f64c9e00f8d
godmook <cmoh4135@naver.com>
2023-07-09 21:49:06 +0900

    50->100!

--- Algorithm_Study/src/Baekjoon/P_5525.java
+++ Algorithm_Study/src/Baekjoon/P_5525.java
@@ -6,23 +6,25 @@ public class P_5525 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String key_str="";
         String a=br.readLine();
         String b=br.readLine();
         String main_str=br.readLine();
         int ans=0;
-        int maxim= (int) Math.floor(Integer.parseInt(b));
-        while(true){
-            key_str="";
-            for(int i=0;i<maxim;i++){
-                key_str+="IO";
-            }
-            key_str+="I";
-            if(main_str.contains(key_str))break;
-            else maxim--;
+        int cnt=0;
+        for(int i=0;i<Integer.parseInt(b)-2;i++){
+            //System.out.println(main_str.substring(i,i+3));
+                if(main_str.substring(i,i+3).equals("IOI")){
+                    cnt++;
+                    i++;
+                    if(cnt==Integer.parseInt(a)){
+                        ans++;
+                        cnt--;
+                    }
+                }
+                else{
+                    cnt=0;
+                }
         }
-        System.out.println(maxim);
-        ans=maxim+1-Integer.parseInt(a);
         bw.write(Integer.toString(ans));
         bw.flush();
     }
