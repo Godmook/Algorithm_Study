cc663df7f43aa57960608bcc0f09f42b99d77716
godmook <cmoh4135@naver.com>
2023-08-21 05:03:48 +0900

    8.21 Study

---
+++ Algorithm_Study/src/Baekjoon/P_14501.java
@@ -0,0 +1,15 @@
+package Baekjoon;
+
+import java.io.*;
+
+public class P_14501 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int a = Integer.parseInt(br.readLine());
+
+        for(int i=0;i<a;i++){
+
+        }
+    }
+}
